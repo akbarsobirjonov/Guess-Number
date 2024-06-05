6a76e2dfe3999ddc18439abdc696ec30f674eb19
Akbar Sobirjonov <115370917+akbarsobirjonov@users.noreply.github.com>
2024-06-06 02:36:22 +0500

    Add files via upload

---
+++ Guess number.py
@@ -0,0 +1,11 @@
+import random
+
+random_number = random.randint(0, 5)
+user_number = input("Guess the number from 0 to 5: ")
+
+if user_number == random_number:
+    print("You are right!")
+else:
+    print("You can't guess this number!")
+    
+print(f"Guess number is:  {random_number}")
